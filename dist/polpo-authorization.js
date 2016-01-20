364cd20d690dcb3e3ff0cca7f4c45729acfce9c0
Bert Casier <bert@ernie.be>
2016-01-20 16:11:07 +0100

    fixed broken build

--- dist/polpo-authorization.js
+++ dist/polpo-authorization.js
@@ -5,8 +5,6 @@
 	
 	angular.module('polpo.authorization', ['ui.router', 'lbServices']);
 })();
-<<<<<<< HEAD
-=======
 /* global angular */
 
 (function(){
@@ -19,8 +17,8 @@
 		
 		$provide.decorator('Person', personDecorator);
 		
-		personDecorator.$inject = ['$delegate', '$rootScope', '$q', 'AuthService'];
-		function personDecorator($delegate, $rootScope, $q, AuthService)
+		personDecorator.$inject = ['$delegate', '$rootScope', '$q', 'AuthService', 'LoopBackAuth', '$location'];
+		function personDecorator($delegate, $rootScope, $q, AuthService, LoopBackAuth, $location)
 		{
 			$delegate.getCurrentUser = function(refresh, cb) {
 				var currentUser = $delegate.getCachedCurrent();
@@ -52,6 +50,18 @@
 				});
 				return promise;
 			};
+
+			if(!$delegate.isAuthenticated()){
+				var params = $location.search();
+
+				// Handle response by adding properties to the LBAuth and then calling save
+				LoopBackAuth.currentUserId = params.userId;
+				LoopBackAuth.accessTokenId = params.accessToken;
+				// Note that you can also set LoopBackAuth.rememberMe which changes the storage from session to local.
+
+				// Saves the values to local storage.
+				LoopBackAuth.save();
+			}
 			
 			return $delegate;
 		}
@@ -61,7 +71,6 @@
 	}
 
 })();
->>>>>>> origin/master
 (function () {
 	'use strict';
 
@@ -469,73 +478,6 @@
 		}
 	}
 })();
-/* global angular */
-
-(function(){
-    'use strict';
-
-	angular.module('polpo.authorization').config(authConfig);
-	
-	authConfig.$inject = ['$provide', 'AuthServiceProvider'];
-    function authConfig($provide, AuthServiceProvider) {
-		
-		$provide.decorator('Person', personDecorator);
-		
-		personDecorator.$inject = ['$delegate', '$rootScope', '$q', 'AuthService', 'LoopBackAuth', '$location'];
-		function personDecorator($delegate, $rootScope, $q, AuthService, LoopBackAuth, $location)
-		{
-			$delegate.getCurrentUser = function(refresh, cb) {
-				var currentUser = $delegate.getCachedCurrent();
-				// allow callback function without refresh parameter
-				if (angular.isFunction(refresh)) {
-					cb = refresh;
-					refresh = false;
-				}
-				
-				if (!refresh && currentUser) {
-					if (cb) {
-						return cb(currentUser);
-					}
-					// always return promise, so we don't have to check
-					return $q.when(currentUser);
-				}
-				
-				// update from backend, returns promise
-				var promise = $delegate.getCurrent({filter: {include: ['personPreferences', 'roles']}}).$promise;
-				promise.then(function(user) {
-					AuthService.user(user);
-					// send global event that user is updated
-					// catch with `$rootScope.$on('user.update', function(e, user) {});´ where needed (f.i. HeaderController)
-					$rootScope.$emit('user.update', user);
-
-					if (cb) {
-						return cb(user);
-					}
-				});
-				return promise;
-			};
-
-			if(!$delegate.isAuthenticated()){
-				var params = $location.search();
-
-				// Handle response by adding properties to the LBAuth and then calling save
-				LoopBackAuth.currentUserId = params.userId;
-				LoopBackAuth.accessTokenId = params.accessToken;
-				// Note that you can also set LoopBackAuth.rememberMe which changes the storage from session to local.
-
-				// Saves the values to local storage.
-				LoopBackAuth.save();
-			}
-			
-			return $delegate;
-		}
-
-		AuthServiceProvider.settings({});
-		
-	}
-<<<<<<< HEAD
-=======
-})();
 (function () {
 	'use strict';
 	
@@ -710,6 +652,5 @@
 		};
 	}
 	/**/
->>>>>>> origin/master
 
 })();
